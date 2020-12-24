4dba492e1257b374368083508523e288ff1f2472
Befa222 <72620186+Befa222@users.noreply.github.com>
2020-12-24 11:53:42 +0000

    added counter

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,25 @@
-import React from 'react'
-
+import React, { useState} from 'react'
 
 
 
 function Home() {
+
+    const [count, setCount] = useState(0)
+
+    function decrementCount() {
+        setCount(prevCount => prevCount -1)
+    }
+    
+    function incrementCount() {
+        setCount(prevCount => prevCount +1)
+    }
+    
     return (
         <div>
             <h1>home page</h1>
+            <button onClick={decrementCount}>-</button>
+            <span>{count}</span>
+            <button onClick={incrementCount}>+</button>
         </div>
     )
 }
